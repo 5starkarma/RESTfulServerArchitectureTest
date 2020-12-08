dab80d2d0312f1c670bd0247f50afe893cfab9f2
5starkarma <davidalford678@gmail.com>
2020-12-07 18:31:11 -0800

    Initial commit

--- config/urls.py
+++ config/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
+    # path('', landing, name='garage'),
+    path('admin/doc/', include('django.contrib.admindocs.urls')),
     path('admin/', admin.site.urls),
+
+    # include all app urls
+    path('', include('apps.companies.urls')),
 ]
