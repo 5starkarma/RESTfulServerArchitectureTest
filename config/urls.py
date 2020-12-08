4c7a434eabd88a67418a88b0e752aafaed5c3a3d
5starkarma <davidalford678@gmail.com>
2020-12-08 06:03:48 -0800

    Refactor

--- config/urls.py
+++ config/urls.py
@@ -16,14 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+from rest_framework_simplejwt.views import (TokenObtainPairView,
+                                            TokenRefreshView,)
 
 
 urlpatterns = [
-    # path('', landing, name='garage'),
     path('admin/doc/', include('django.contrib.admindocs.urls')),
     path('admin/', admin.site.urls),
 
