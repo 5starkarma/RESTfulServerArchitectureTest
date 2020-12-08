6399787d642accde293e11877101f6fd289f1e37
5starkarma <davidalford678@gmail.com>
2020-12-08 05:32:58 -0800

    Create BoatAPIView

--- apps/boats/urls.py
+++ apps/boats/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from apps.boats.views import BoatAPIView
 
 urlpatterns = [
-    path('trucks/', include([
+    path('boats/', include([
         path('', BoatAPIView.as_view(), name='boats_list_api'),
         path('<int:id>/', BoatAPIView.as_view(), name='boat_detail_api'),
     ])),
