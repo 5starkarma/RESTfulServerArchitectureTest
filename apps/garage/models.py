4cbdc2a35a2f16de29d4415c66c76f7517953584
5starkarma <davidalford678@gmail.com>
2020-12-07 18:30:39 -0800

    Add Garage app

--- apps/garage/models.py
+++ apps/garage/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 
-# Cars
 
-# Trucks
-
-# Boats
+# Garage
+class Garage(models.Model):
+    # car = models.ManyToManyField(Car, related_name='cars_garaged')
+    # truck = models.ManyToManyField(Truck, related_name='trucks_garaged')
+    # boat = models.ManyToManyField(Boat, related_name='boats_garaged')
+    pass
