67405f663b70ded17ad2da2b48267939ab00bf5f
5starkarma <davidalford678@gmail.com>
2020-12-08 09:14:03 -0800

    Add VehicleYearValidator to serializers.py

--- apps/cars/serializers.py
+++ apps/cars/serializers.py
@@ -1,8 +1,12 @@
 from rest_framework import serializers
+
 from apps.cars.models import Car
+from apps.utils.validators import VehicleYearValidator
 
 
 class CarSerializer(serializers.ModelSerializer):
     class Meta:
         model = Car
         fields = '__all__'
+
+    year = serializers.IntegerField(validators=[VehicleYearValidator()])
--- apps/trucks/serializers.py
+++ apps/trucks/serializers.py
@@ -1,9 +1,12 @@
 from rest_framework import serializers
 
 from apps.trucks.models import Truck
+from apps.utils.validators import VehicleYearValidator
 
 
 class TruckSerializer(serializers.ModelSerializer):
     class Meta:
         model = Truck
         fields = '__all__'
+
+    year = serializers.IntegerField(validators=[VehicleYearValidator()])
